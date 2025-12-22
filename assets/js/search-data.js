899be7638532df011183789d26b2b8ab74f43e54
emanbuc <emanuele.buchicchio@gmail.com>
2025-12-22 14:01:45 +0000

    Deploying to gh-pages from @ emanbuc/emanbuc.github.io@ba36c13f3bf4007f178cb9b43f92c8b1eb222f76 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,7 +508,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%65%6D%61%6E%75%65%6C%65.%62%75%63%68%69%63%63%68%69%6F@%69%65%65%65.%6F%72%67", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/emanbuc", "_blank");
+        },
+      },{
+        id: 'social-ieee',
+        title: 'IEEE Xplore',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://ieeexplore.ieee.org/author/37088929353/", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -516,6 +530,27 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/emanuelebuchicchio", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-2786-5931", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/Emanuele_Buchicchio2/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -532,11 +567,18 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
+        id: 'social-scopus',
+        title: 'Scopus',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.scopus.com/authid/detail.uri?authorId=57259757300", "_blank");
+        },
+      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://emerlab.ing.unipg.it/", "_blank");
         },
       },{
       id: 'light-theme',
