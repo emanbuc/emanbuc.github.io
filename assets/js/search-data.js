6b598bfed7eb4fa8bf815e2ad001a1b4c8296f5b
emanbuc <emanuele.buchicchio@gmail.com>
2025-12-26 10:30:02 +0000

    Deploying to gh-pages from @ emanbuc/emanbuc.github.io@b4fabf4b1a57305fbbb2ca8d01e7871113a513e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,28 +105,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/hosting-website-github/";
           
         },
-      },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/plotly/";
-          
-        },
-      },{id: "post-a-post-with-image-galleries",
-        
-          title: "a post with image galleries",
-        
-        description: "this is what included image galleries could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/photo-gallery/";
-          
-        },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -136,226 +114,6 @@ ninja.data = [{
         handler: () => {
           
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-a-post-with-tabs",
-        
-          title: "a post with tabs",
-        
-        description: "this is what included tabs in a post could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/tabs/";
-          
-        },
-      },{id: "post-a-post-with-typograms",
-        
-          title: "a post with typograms",
-        
-        description: "this is what included typograms code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/typograms/";
-          
-        },
-      },{id: "post-a-post-that-can-be-cited",
-        
-          title: "a post that can be cited",
-        
-        description: "this is what a post that can be cited looks like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/post-citation/";
-          
-        },
-      },{id: "post-a-post-with-pseudo-code",
-        
-          title: "a post with pseudo code",
-        
-        description: "this is what included pseudo code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/pseudocode/";
-          
-        },
-      },{id: "post-a-post-with-code-diff",
-        
-          title: "a post with code diff",
-        
-        description: "this is how you can display code diffs",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/code-diff/";
-          
-        },
-      },{id: "post-a-post-with-advanced-image-components",
-        
-          title: "a post with advanced image components",
-        
-        description: "this is what advanced image components could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/advanced-images/";
-          
-        },
-      },{id: "post-a-post-with-vega-lite",
-        
-          title: "a post with vega lite",
-        
-        description: "this is what included vega lite code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/vega-lite/";
-          
-        },
-      },{id: "post-a-post-with-geojson",
-        
-          title: "a post with geojson",
-        
-        description: "this is what included geojson code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/geojson-map/";
-          
-        },
-      },{id: "post-a-post-with-echarts",
-        
-          title: "a post with echarts",
-        
-        description: "this is what included echarts code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/echarts/";
-          
-        },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/chartjs/";
-          
-        },
-      },{id: "post-a-post-with-tikzjax",
-        
-          title: "a post with TikZJax",
-        
-        description: "this is what included TikZ code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tikzjax/";
-          
-        },
-      },{id: "post-a-post-with-bibliography",
-        
-          title: "a post with bibliography",
-        
-        description: "an example of a blog post with bibliography",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/post-bibliography/";
-          
-        },
-      },{id: "post-a-post-with-jupyter-notebook",
-        
-          title: "a post with jupyter notebook",
-        
-        description: "an example of a blog post with jupyter notebook",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/jupyter-notebook/";
-          
-        },
-      },{id: "post-a-post-with-custom-blockquotes",
-        
-          title: "a post with custom blockquotes",
-        
-        description: "an example of a blog post with custom blockquotes",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/custom-blockquotes/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
-        
-          title: "a post with table of contents on a sidebar",
-        
-        description: "an example of a blog post with table of contents on a sidebar",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/sidebar-table-of-contents/";
-          
-        },
-      },{id: "post-a-post-with-audios",
-        
-          title: "a post with audios",
-        
-        description: "this is what included audios could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/audios/";
-          
-        },
-      },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/videos/";
-          
-        },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tables/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents",
-        
-          title: "a post with table of contents",
-        
-        description: "an example of a blog post with table of contents",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/table-of-contents/";
-          
-        },
-      },{id: "post-a-post-with-giscus-comments",
-        
-          title: "a post with giscus comments",
-        
-        description: "an example of a blog post with giscus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2022/giscus-comments/";
           
         },
       },{id: "post-displaying-external-posts-on-your-al-folio-blog",
@@ -369,44 +127,22 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "post-a-post-with-redirect",
+      },{id: "post-differential-privacy",
         
-          title: "a post with redirect",
+          title: "Differential Privacy",
         
-        description: "you can also redirect to assets like pdf",
+        description: "Differential privacy is a leading edge approach that enables useful analysis while protecting individually identifiable data values",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/assets/pdf/example_pdf.pdf";
+            window.location.href = "/blog/2021/Differential_Privacy/";
           
         },
-      },{id: "post-a-post-with-diagrams",
+      },{id: "post-azure-machine-learning-experiments",
         
-          title: "a post with diagrams",
+          title: "Azure Machine Learning experiments",
         
-        description: "an example of a blog post with diagrams",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/diagrams/";
-          
-        },
-      },{id: "post-a-distill-style-blog-post",
-        
-          title: "a distill-style blog post",
-        
-        description: "an example of a distill-style blog post and main elements",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/distill/";
-          
-        },
-      },{id: "post-azureml-exeperiments",
-        
-          title: "Azureml_exeperiments",
-        
-        description: "",
+        description: "In Azure Machine Learning, an experiment is a named process, usually the running of a script or a pipeline, that can generate metrics and outputs and be tracked in the Azure Machine Learning workspace",
         section: "Posts",
         handler: () => {
           
@@ -415,156 +151,68 @@ ninja.data = [{
         },
       },{id: "post-azure-machine-learning",
         
-          title: "Azure_machine_learning",
+          title: "Azure Machine Learning",
         
-        description: "",
+        description: "Azure Machine Learning is a cloud-based platform for building and operating machine learning solutions in Azure.",
         section: "Posts",
         handler: () => {
           
             window.location.href = "/blog/2021/Azure_Machine_Learning/";
           
         },
-      },{id: "post-azureml-automated-machine-learning",
+      },{id: "post-azure-machine-learning-hyperparameter-tuning",
         
-          title: "Azureml_automated_machine_learning",
+          title: "Azure Machine Learning - Hyperparameter Tuning",
         
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/AzureML_Automated_Machine_Learning/";
-          
-        },
-      },{id: "post-create-azure-sql-db-from-data-export",
-        
-          title: "Create Azure Sql Db From Data Export",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/create-azure-sql-db-from-data-export/";
-          
-        },
-      },{id: "post-hyperparameter-tuning",
-        
-          title: "Hyperparameter Tuning",
-        
-        description: "",
+        description: "The choice of hyperparameter values can significantly affect the resulting model, making it important to select the best possible values.",
         section: "Posts",
         handler: () => {
           
             window.location.href = "/blog/2021/Hyperparameter-tuning/";
           
         },
-      },{id: "post-differential-privacy",
+      },{id: "post-machine-learning-and-big-data-course-outline",
         
-          title: "Differential_privacy",
+          title: "Machine Learning and Big Data Course Outline",
         
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/Differential_Privacy/";
-          
-        },
-      },{id: "post-big-data-meet-ml",
-        
-          title: "Big_data_meet_ml",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/big_data_meet_ml/";
-          
-        },
-      },{id: "post-machine-learning-and-big-data",
-        
-          title: "Machine_learning_and_big_data",
-        
-        description: "",
+        description: "Machine Learning and Big Data lecture topics",
         section: "Posts",
         handler: () => {
           
             window.location.href = "/blog/2021/Machine_Learning_And_Big_Data/";
           
         },
-      },{id: "post-ai-at-scale",
+      },{id: "post-machine-learning-ml-meets-big-data",
         
-          title: "Ai_at_scale",
+          title: "Machine Learning (ML) Meets Big Data",
         
-        description: "",
+        description: "data represents a gold mine in terms of commercial value and also important reference material for policy makers. But much of this value will stay unexploited until the tools for processing the huge amount of information remain unavailable",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/big_data_meet_ml/";
+          
+        },
+      },{id: "post-machine-learning-ml-meets-big-data",
+        
+          title: "Machine Learning (ML) Meets Big Data",
+        
+        description: "Over the past years, large deep learning language models (LM) with billions of parameters have improved the state-of-the-art in nearly every DL application.",
         section: "Posts",
         handler: () => {
           
             window.location.href = "/blog/2021/AI_at_scale/";
           
         },
-      },{id: "post-a-post-with-twitter",
+      },{id: "post-migrate-an-existing-sql-database-from-local-server-to-azure-sql",
         
-          title: "a post with twitter",
+          title: "Migrate an existing SQL database from local server to Azure SQL",
         
-        description: "an example of a blog post with twitter",
+        description: "There are specific tools you can use to migrate an existing database to Azure SQL.",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2020/twitter/";
-          
-        },
-      },{id: "post-a-post-with-disqus-comments",
-        
-          title: "a post with disqus comments",
-        
-        description: "an example of a blog post with disqus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/disqus-comments/";
-          
-        },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/math/";
-          
-        },
-      },{id: "post-a-post-with-code",
-        
-          title: "a post with code",
-        
-        description: "an example of a blog post with some code",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/code/";
-          
-        },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/images/";
-          
-        },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/formatting-and-links/";
+            window.location.href = "/blog/2021/create-azure-sql-db-from-data-export/";
           
         },
       },{id: "books-the-godfather",
